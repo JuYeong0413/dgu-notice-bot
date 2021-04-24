b7803fc77d3657bd5e958d5269f26551bd0997d8
Juyeong Lee <2jy22@naver.com>
2021-04-24 23:13:40 +0900

    오늘 올라온 공지 제목 가져오기

--- crawling_scholarship.py
+++ crawling_scholarship.py
@@ -2,32 +2,51 @@ import requests
 from bs4 import BeautifulSoup
 from datetime import datetime, timedelta
 from pytz import timezone
+import re
 
 url = 'https://www.dongguk.edu/mbs/kr/jsp/board/list.jsp?boardId=3662&id=kr_010804000000' # 장학공지
 
-html = requests.get(url)
+def get_url_list():
+    html = requests.get(url)
 
-soup = BeautifulSoup(html.text, 'html.parser')
-table = soup.find(id="board_list")
-rows = table.find_all('tr')[1:]
+    soup = BeautifulSoup(html.text, 'html.parser')
+    table = soup.find(id="board_list")
+    rows = table.find_all('tr')[1:]
 
-# 상단 고정 공지 제외
-today = datetime.now(timezone('Asia/Seoul')).strftime('%Y-%m-%d')
-# today = (datetime.today() - timedelta(days=2)).strftime('%Y-%m-%d') # For test
+    # 상단 고정 공지 제외
+    today = datetime.now(timezone('Asia/Seoul')).strftime('%Y-%m-%d')
+    # today = (datetime.today() - timedelta(days=2)).strftime('%Y-%m-%d') # For test
 
-start = 0
-end = 0
-for i in rows:
-    if i.td.img is None:
-        if today == i.select('td')[3].text.strip():
-            end += 1
-    else:
-        start += 1
+    start = 0
+    end = 0
+    for i in rows:
+        if i.td.img is None:
+            if today == i.select('td')[3].text.strip():
+                end += 1
+        else:
+            start += 1
 
-end += start
-today_notice = rows[start:end]
+    end += start
+    today_notice = rows[start:end]
 
-# 오늘 올라온 공지 url
-today_notice_url = []
-for i in today_notice:
-    today_notice_url.append('http://www.dongguk.edu/mbs/kr/jsp/board/' + i.a['href'].strip())
+    # 오늘 올라온 공지 url
+    today_notice_url = []
+    for i in today_notice:
+        today_notice_url.append('http://www.dongguk.edu/mbs/kr/jsp/board/' + i.a['href'].strip())
+
+    return today_notice_url
+
+def get_notice_list(urls):
+    notice_list = []
+    for url in urls:
+        html = requests.get(url)
+        soup = BeautifulSoup(html.text, 'html.parser')
+        table = soup.find(id="board_view")
+        title_text = table.select_one('th').text.strip()
+        title = re.sub(r'\s+', ' ', title_text)
+        print(title)
+
+
+if __name__ == "__main__":
+    urls = get_url_list()
+    get_notice_list(urls)
