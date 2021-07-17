832fdc3a83b626dfeecdc371483e22df2617020c
Juyeong Lee <2jy22@naver.com>
2021-07-17 18:53:24 +0900

    테스트 채널 메세지 전송 리팩토링

--- discord_bot.py
+++ discord_bot.py
@@ -62,21 +62,16 @@ async def react_test(ctx):
     scholarship_message = crawling.run(scholarship_url, type_scholarship)
     cse_message = cse_crawling.run(cse_url, type_cse)
 
-    # Send message to channel
-    ch_general = client.get_channel(835518591830851584)
-    await ch_general.send(general_message)
-    ch_academic = client.get_channel(835518591830851584)
-    await ch_academic.send(academic_message)
-    ch_scholarship = client.get_channel(835518591830851584)
-    await ch_scholarship.send(scholarship_message)
-    ch_cse = client.get_channel(835518591830851584)
-    await ch_cse.send(cse_message)
+    # Send message to test channel
+    test_channel = client.get_channel(835518591830851584)
+    await test_channel.send(general_message)
+    await test_channel.send(academic_message)
+    await test_channel.send(scholarship_message)
+    await test_channel.send(cse_message)
     
     print(client.user.name, 'successfully sent notices to test channel.')
     await client.close()
     print(client.user.name, 'successfully logged out.')
-
-    return None
 
 
 @client.event
@@ -90,7 +85,6 @@ async def on_command_error(ctx, error):
     await client.close()
     print(client.user.name, 'successfully logged out.')
 
-    return None
 
 # client.run(secrets.get_token()) # For local test
 client.run(token)
